ef81063bdb0422bc5d4af54a3454d9e5b045e94f
veinskiy.v <veinskiy.v@wildberries.ru>
2020-10-12 23:10:41 +0300

    added cors

--- cmd/handlers/middleware.go
+++ cmd/handlers/middleware.go
@@ -1,6 +1,9 @@
 package handlers
 
+import "github.com/valyala/fasthttp"
+
 type Middleware interface {
+	CORS(next fasthttp.RequestHandler) fasthttp.RequestHandler
 }
 
 type middleware struct {
@@ -8,4 +11,23 @@ type middleware struct {
 
 func NewMiddleware() Middleware {
 	return middleware{}
+}
+
+var (
+	corsAllowHeaders     = "authorization"
+	corsAllowMethods     = "HEAD,GET,POST,PUT,DELETE,OPTIONS"
+	corsAllowOrigin      = "*"
+	corsAllowCredentials = "true"
+)
+
+func (m middleware) CORS(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+
+		ctx.Response.Header.Set("Access-Control-Allow-Credentials", corsAllowCredentials)
+		ctx.Response.Header.Set("Access-Control-Allow-Headers", corsAllowHeaders)
+		ctx.Response.Header.Set("Access-Control-Allow-Methods", corsAllowMethods)
+		ctx.Response.Header.Set("Access-Control-Allow-Origin", corsAllowOrigin)
+
+		next(ctx)
+	}
 }
--- cmd/handlers/router.go
+++ cmd/handlers/router.go
@@ -6,16 +6,16 @@ import (
 	"github.com/buaazp/fasthttprouter"
 )
 
-func NewFastHttpRouter(posts postsHandler.Handler, upload uploadHandler.Handler,  middleware Middleware) *fasthttprouter.Router {
+func NewFastHttpRouter(posts postsHandler.Handler, upload uploadHandler.Handler, middleware Middleware) *fasthttprouter.Router {
 	router := fasthttprouter.New()
 
 	//router.Handle("GET", "/health", check)
 
-	router.Handle("POST", "/api/posts/post", posts.Create)
-	router.Handle("GET", "/api/posts/posts", posts.GetList)
+	router.Handle("POST", "/api/posts/post", middleware.CORS(posts.Create))
+	router.Handle("GET", "/api/posts/posts", middleware.CORS(posts.GetList))
 
-	router.Handle("POST", "/api/upload/photo", upload.Photo)
-	router.Handle("POST", "/api/upload/file", upload.File)
+	router.Handle("POST", "/api/upload/photo", middleware.CORS(upload.Photo))
+	router.Handle("POST", "/api/upload/file", middleware.CORS(upload.File))
 
 	return router
 }
