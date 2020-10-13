2a5bae51c0531eeb1a89982af742025d49cff6e1
Andrey Kochnov <a.kochnov@corp.mail.ru>
2020-10-14 02:01:05 +0300

    Panic handler

--- cmd/handlers/router.go
+++ cmd/handlers/router.go
@@ -1,15 +1,20 @@
 package handlers
 
 import (
+	"fmt"
 	postsHandler "github.com/Solar-2020/SolAr_2020/cmd/handlers/posts"
 	uploadHandler "github.com/Solar-2020/SolAr_2020/cmd/handlers/upload"
+	"github.com/Solar-2020/SolAr_2020/internal/errorWorker"
 	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
 )
 
 func NewFastHttpRouter(posts postsHandler.Handler, upload uploadHandler.Handler, middleware Middleware) *fasthttprouter.Router {
 	router := fasthttprouter.New()
 
 	//router.Handle("GET", "/health", check)
+
+	router.PanicHandler = panicHandler
 
 	router.Handle("POST", "/api/posts/post", middleware.CORS(posts.Create))
 	router.Handle("GET", "/api/posts/posts", middleware.CORS(posts.GetList))
@@ -18,4 +23,9 @@ func NewFastHttpRouter(posts postsHandler.Handler, upload uploadHandler.Handler,
 	router.Handle("POST", "/api/upload/file", middleware.CORS(upload.File))
 
 	return router
+}
+
+func panicHandler(ctx *fasthttp.RequestCtx, err interface{}) {
+	fmt.Printf("Request falied with panic: %s, error: %v", string(ctx.Request.RequestURI()) , err)
+	errorWorker.NewErrorWorker().ServeFatalError(ctx)
 }
